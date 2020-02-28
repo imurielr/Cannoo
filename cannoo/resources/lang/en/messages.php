59dc28e141cd2486bd0b613662c5fc24002bf8a8
Isabela <imurielr@eafit.edu.co>
2020-02-28 16:52:47 -0500

    Adding product controller, model, views and routes. Also adding lang in spanish and english

---
+++ cannoo/resources/lang/en/messages.php
@@ -0,0 +1,17 @@
+<?php 
+
+    return [
+        'create' => 'Create',
+        'createProduct' => 'Create product',
+        'products' => 'Products',
+        'type_form' => 'Enter type',
+        'price_form' => 'Enter price',
+        'description_form' => 'Enter a description',
+        'send' => 'send',
+        'product' => 'Producto',
+        'price' => 'Price',
+        'description' => 'Description',
+        'changeDescription' => 'Change Description',
+        'deleteProduct' => 'Delete Product',
+        'updateDescription' => 'Upate Description',
+    ];
---
+++ cannoo/resources/views/product/update.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master')
+
+@section("title", $data["title"])
+
+@section('content')
+<div class="container">
+    <div class="row p-5">
+        <div class="col-md-12">
+            <ul id="errors">
+                <div class="card">
+                    <div class="card-header">{{ $data["product"]->getType() }}</div>
+                    <div class="card-body">
+                        <form method="POST" action="{{ url('product/update/' . $data['product']->getId()) }}">
+                            @csrf
+                            <input type="text" placeholder="@lang('messages.description_form')" name="description" value="{{ old('description') }}" />
+                            <input type="submit" value="@lang('messages.updateDescription')" />
+                        </form>
+                    </div>
+                </div>
+                <br/>
+            </ul>
+        </div>
+    </div>
+</div>
+@endsection
