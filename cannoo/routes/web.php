ee3822d7e6109dc7d2cc7e09da5c4a87b73c6aa1
imurielr <imurielr@eafit.edu.co>
2020-02-28 17:46:39 -0500

    Update web.php

--- cannoo/routes/web.php
+++ cannoo/routes/web.php
@@ -11,12 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/index', 'HomeController@index')->name("home.index");
-
 Route::get('/product/create', 'ProductController@create')->name("product.create");
 Route::post('/product/save', 'ProductController@save')->name("product.save");
 Route::get('/product/show', 'ProductController@show')->name("product.show");
@@ -24,6 +20,4 @@ Route::get('/product/show/{id}', 'ProductController@showProduct')->name("product
 Route::get('/product/delete/{id}', 'ProductController@delete')->name("product.delete");
 Route::get('/product/updateDescription/{id}', 'ProductController@changeDescription')->name("product.update");
 Route::post('/product/update/{id}', 'ProductController@update')->name("product.updateDescription");
-
-Route::get('/index', 'HomeController@index')->name("home.index");
 Route::get('/certificate/show/{id}', 'CertificateController@get')->name("certificate.show");
