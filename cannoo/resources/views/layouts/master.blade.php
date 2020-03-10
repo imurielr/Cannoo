b20f2232824f220ba6718f4b4422fda0b782b555
Isabela <imurielr@eafit.edu.co>
2020-03-10 11:41:36 -0500

    Modificando permisos, administradores no pueden realizar compras

--- cannoo/resources/views/layouts/master.blade.php
+++ cannoo/resources/views/layouts/master.blade.php
@@ -106,7 +106,9 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('order.index') }}">@lang('messages.shoppingCart')</a>
+                                @if (Auth::user()->role != 'admin')
+                                    <a class="dropdown-item" href="{{ route('order.index') }}">@lang('messages.shoppingCart')</a>
+                                @endif
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
                                                     document.getElementById('logout-form').submit();">
